68d2f6b493e5dfa404db71375f5fe19c803f1957
Geoff George <52931060+geoffgeorgein@users.noreply.github.com>
2022-08-21 11:24:27 +0530

    Update app.js

--- To_Do_List/app.js
+++ To_Do_List/app.js
@@ -1,53 +1,144 @@
+//jshint esversion:6
 
-const express=require('express');
-// const ejs=require('ejs');
-const https=require('https');
-// const request=require("request");
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose=require("mongoose");
 
-const app=express();
+const app = express();
 
-app.set("view engine","ejs");
+app.set('view engine', 'ejs');
 
-const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static('public'));
-
-var items=["Eat","Code","Sleep"];
-var item="";
-
-app.get('/',(req,res)=>{
-
-    // res.send('Hello');
-
-    var today=new Date();
-    var currentday=today.getDay();
-
-    var options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-
-    };
+mongoose.connect('mongodb://localhost:27017/Itemsdb',{useNewUrlParser:true});
 
 
-    var day=today.toLocaleDateString("en-US",options);
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+const itemSchema=new mongoose.Schema({
+
+  name:String,
+ 
+});
+
+const Item=mongoose.model("Item",itemSchema);
+
+const item1=new Item({
+
+    name:"SLeep",
+    
+    
+});
+const item2=new Item({
+
+  name:"Play",
+  
+  
+});
+
+// Item.insertMany([item1,item2],(err)=>{
+
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("insertion successful");
+//     }
+// });
+
+// Item.find((err,items)=>{
+
+//   if(err){
+//       console.log("error");
+//   }
+//   else{
+
+//     if(items.length==0){
+
+      
+//     }
+      
+//      // mongoose.connection.close();
+
+//       items.forEach((i) => {
+//           console.log(i.name);
+//       });
+//   }
+// })
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+  Item.find((err,items)=>{
+
+    if(err){
+        console.log("error");
+    }
+    else{
+
+      if(items.length==0){
+
+        Item.insertMany([item1,item2],(err)=>{
+
+          if(err){
+              console.log(err);
+          }
+          else{
+              console.log("insertion successful");
+          }
+        });
+        res.redirect('/');
+      }
+      
+      else{
+          res.render("list", {listTitle: day, newListItems: items});
+      }
+        
+      // mongoose.connection.close();
+
+        
+    }
+  })
 
   
-    res.render("list",{kday:day,newlistitems:items});
-})
 
-app.post('/',(req,res)=>{
+});
 
-    var item=(req.body.text1);
-    items.push(item);
-    res.redirect('/');
-})
+app.post("/", function(req, res){
 
+  const item = req.body.newItem;
 
-// rlet port = process.env.PORT;
+  const item4=new Item({
+
+    name:item,
     
-     
-    app.listen(3000, function() {
-      console.log("Server started succesfully");
-    });   
+    
+  });
+
+  Item.insertMany([item4],(err)=>{
+
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Inserted one item successfully");
+        res.redirect('/');
+    }
+  });
+  
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+})
